4770bc8858483f812fd81b460652597414bd40f5
Byungkook Jung <byungkookjung@Byungkooks-MacBook-Air.local>
2024-09-25 19:26:16 -0600

    i

--- app/src/main/java/com/example/jobber/Star.java
+++
@@ -1,5 +0,0 @@
-package com.example.jobber;
-import com.example.jobber.Shape
-
-public class Star extends Shape{
-}
---
+++ app/src/main/java/com/example/jobber/Tours.java
@@ -0,0 +1,4 @@
+package com.example.jobber;
+
+public class Tours extends Shape{
+}
